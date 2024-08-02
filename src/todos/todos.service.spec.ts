e1cd9b43f8d40106dc44c12cbce79cc5b82c54d5
ludndev <ludndev@gmail.com>
2024-08-02 16:18:41 +0100

    feat: add test to check all todos retrieval

--- src/todos/todos.service.spec.ts
+++ src/todos/todos.service.spec.ts
@@ -1,18 +1,62 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodosService } from './todos.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { PrismaClient } from '@prisma/client';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import now = jest.now;
 
 describe('TodosService', () => {
   let service: TodosService;
+  let prisma: DeepMockProxy<PrismaClient>;
+
+  const testTodos = [
+    {
+      id: 1,
+      content: 'test content',
+      status: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 2,
+      content: 'test content',
+      status: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 3,
+      content: 'test content',
+      status: true,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      id: 4,
+      content: 'test content',
+      status: false,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+  ];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TodosService],
-    }).compile();
+      providers: [TodosService, PrismaService],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
-    service = module.get<TodosService>(TodosService);
+    service = module.get(TodosService);
+    prisma = module.get(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should return all todos', async () => {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    prisma.todo.findMany.mockResolvedValue(testTodos); // why those rules make it work ?
+
+    await expect(service.findAll()).resolves.toEqual(testTodos);
   });
 });
