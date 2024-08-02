ffd6f286fb8cc6ad085aaff875080a499ce774dc
ludndev <ludndev@gmail.com>
2024-08-02 15:56:00 +0100

    feat: add content assertion test for root route

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,10 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(
+        JSON.stringify({
+          message: 'Hello World!',
+        }),
+      );
   });
 });
