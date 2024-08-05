b362b0beaf257c378cb1360dc71700c7a3d1e33c
ludndev <ludndev@gmail.com>
2024-08-05 11:58:47 +0100

    feat: enable validation pipe in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ResponseInterceptor } from './response/response.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,8 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalInterceptors(new ResponseInterceptor());
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
