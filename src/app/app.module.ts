771848e501629f96992fdffb5b82bb6280dc2b63
ludndev <ludndev@gmail.com>
2024-08-05 10:39:31 +0100

    fix: todos dependencies injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodosController } from '../todos/todos.controller';
+import { TodosService } from '../todos/todos.service';
+import { PrismaService } from '../prisma/prisma.service';
+import { TodosModule } from '../todos/todos.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodosModule, PrismaModule],
+  controllers: [AppController, TodosController],
+  providers: [AppService, TodosService, PrismaService],
 })
 export class AppModule {}
