99d87083379463fdc389117c2bc5e2d11065880d
ludndev <ludndev@gmail.com>
2024-08-05 12:19:41 +0100

    feat: add user login dto

---
+++ src/users/dto/user-login.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class UserLoginDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  // // regex for password to contain at least one uppercase, lowercase, number and special character
+  // @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+  //   message:
+  //     'password must contain uppercase, lowercase, number and special character',
+  // })
+  password: string;
+
+  @ApiProperty()
+  @IsEmail()
+  email: string;
+}
